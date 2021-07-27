181aa55f5f510cbd5b7c18209ce7a45db223930a
fbarba9774 <88018106+fbarba9774@users.noreply.github.com>
2021-07-27 10:10:41 -0700

    Update screenconnect.ps1

--- screenconnect.ps1
+++ screenconnect.ps1
@@ -1,4 +1,4 @@
-$url = 'https://p1data-my.sharepoint.com/:u:/g/personal/chrish_premier-one_com/EQdi9nFoB3RLghK0vFqqEIUBRJhgW-I1qbQb4z4rLdHIGA?e=UekwZl'
+$url = 'https://p1data-my.sharepoint.com/:u:/g/personal/chrish_premier-one_com/EQdi9nFoB3RLghK0vFqqEIUBRJhgW-I1qbQb4z4rLdHIGA?e=UekwZl&download=1'
 
 $filename = "SCAgent.exe"
 $outpath = "C:\Windows\Temp"
